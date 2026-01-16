1928eabe77385b9371bc1a8e66caf5d6ab0df681
KOSHINOawa <2381068747@qq.com>
2026-01-17 00:37:22 +0800

    fix some little bug
    When KOSHINO was using AI to search for bugs and attempting to fix them, he placed an absolute path in one place. I then changed it back to a relative path.

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -40,7 +40,8 @@ import TWInvalidProjectModal from '../../containers/tw-invalid-project-modal.jsx
 import TWWindChimeSubmitter from '../../containers/tw-windchime-submitter.jsx';
 import CustomThemeModal from '../../containers/tw-custom-theme-modal.jsx';
 import AEReadMe from '../../containers/ae-readme.jsx'
-import { loadData } from 'C:/AstraEditor/scratch-gui/src/components/ae-readme/ae-readme.jsx'
+import { loadData } from '../ae-readme/ae-readme.jsx'
+// 你根本不知道看到这原本是个绝对路径有多好笑
 import ExtensionManager from '../extension-chooser/extension-chooser.jsx';
 import PreviewExt from '../../containers/ae-preview-ext.jsx';
 
