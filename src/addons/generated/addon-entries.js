f43dcd8a55eb8e99896ce741251eac966b6d106b
KOSHINOawa <2381068747@qq.com>
2026-01-21 20:47:45 +0800

    NEW ADDON
    I make a new addon for Astra Editor: hide toolbar!
    What is a toolbar? Anyway, you can't use it normally! So just hide it! Without eyes, without heart~

--- src/addons/generated/addon-entries.js
+++ src/addons/generated/addon-entries.js
@@ -82,4 +82,5 @@ export default {
   "tw-disable-compiler": () => import(/* webpackChunkName: "addon-entry-tw-disable-compiler" */ "../addons/tw-disable-compiler/_runtime_entry.js"),
   "editor-stepping": () => import(/* webpackChunkName: "addon-entry-editor-stepping" */ "../addons/editor-stepping/_runtime_entry.js"),
   "tw-comment-markdown-editor": () => import(/* webpackChunkName: "addon-entry-tw-comment-markdown-editor" */ "../addons/tw-comment-markdown-editor/_runtime_entry.js"),
+  "hide-toolbar": () => import(/* webpackChunkName: "addon-entry-tw-hide-toolbar" */ "../addons/hide-toolbar/_runtime_entry.js")
 };
