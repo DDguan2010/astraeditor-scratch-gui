e654c70634cfa7b3eea197deabbae90f5a694baa
KOSHINO <2381068747@qq.com>
2026-02-07 02:35:54 +0800

    Change branch to master

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -16,7 +16,7 @@ import LibraryComponent from '../components/library/library.jsx';
 import extensionIcon from '../components/action-menu/icon--sprite.svg';
 const extensionsLibs = [
     { id: "tw", url: "https://extensions.turbowarp.org/" },
-    { id: "ae", url: "https://raw.githubusercontent.com/AstraEditor/extensions/refs/heads/Develop/build" }
+    { id: "ae", url: "https://raw.githubusercontent.com/AstraEditor/extensions/refs/heads/master/build" }
 ]
 const messages = defineMessages({
     extensionTitle: {
